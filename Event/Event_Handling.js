2183770868eae31d8d7674aeb2dc6e93b7a7dba4
Weston128 <weston.haveman@hgs.hiddengeniusproject.org>
2025-10-30 07:29:46 -0400

    Add files via upload

---
+++ Event/Event_Handling.js
@@ -0,0 +1,5 @@
+document.getElementById('myButton').addEventListener('click', function() { //Research the mouseover event and replace it within the .addEventListener method
+    image = document.createElement('img');
+    image.src = 'https://www.hiddengeniusproject.org/wp-content/uploads/2023/01/2023-Website-HGPAssets_SecondaryLogo_Yellow.png'; //change the image to something else 
+    document.body.appendChild(image);
+}); 
